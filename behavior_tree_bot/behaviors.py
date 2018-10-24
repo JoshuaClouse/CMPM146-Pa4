2ebbe311e06bb95195567de88a8bcf0cef2c8166
jclouse <jclouse@ucsc.edu>
2018-10-23 20:03:35 -0700

    want to get rid of these changes in the merge

--- behavior_tree_bot/behaviors.py
+++ behavior_tree_bot/behaviors.py
@@ -1,11 +1,12 @@
 import sys
+#logging.basicConfig(filename=__file__[:-3] +'.log', filemode='w', level=logging.DEBUG)
 sys.path.insert(0, '../')
 from planet_wars import issue_order
 
 
 def attack_weakest_enemy_planet(state):
     # (1) If we currently have a fleet in flight, abort plan.
-    if len(state.my_fleets()) >= 1:
+    if len(state.my_fleets()) >= 3:
         return False
 
     # (2) Find my strongest planet.
@@ -24,7 +25,8 @@ def attack_weakest_enemy_planet(state):
 
 def spread_to_weakest_neutral_planet(state):
     # (1) If we currently have a fleet in flight, just do nothing.
-    if len(state.my_fleets()) >= 1:
+    #logging.debug("please display")
+    if len(state.my_fleets()) >= 3:
         return False
 
     # (2) Find my strongest planet.
@@ -39,3 +41,25 @@ def spread_to_weakest_neutral_planet(state):
     else:
         # (4) Send half the ships from my strongest planet to the weakest enemy planet.
         return issue_order(state, strongest_planet.ID, weakest_planet.ID, strongest_planet.num_ships / 2)
+
+def spread_to_closest_neutral_planet(state):
+
+    if len(state.my_fleets()) >= 3:
+        return False
+    distance = None
+    closestNeutralPlanet = None
+    closestOwnedPlanet = None
+    logging.debug("please display")
+    for neutral_planet in state.neutral_planets():
+        for my_planet in state.my_planets():
+            if neutral_planet.num_ships < my_planet.numships * .7:
+                if closestNeutralPlanet == None:
+                    closestNeutralPlanet = neutral_planet
+                    closestOwnedPlanet = my_planet
+                    distance = state.distance(my_planet.ID, neutral_planet.ID)
+                elif state.distance(my_planet.ID, neutral_planet.ID) < distance:
+                    closestNeutralPlanet = neutral_planet
+                    closestOwnedPlanet = my_planet
+                    distance = state.distance(my_planet.ID, neutral_planet.ID)
+    return issue_order(state, closestOwnedPlanet.ID, closestNeutralPlanet.ID, closestOwnedPlanet.num_ships * .7)
+    return False
--- behavior_tree_bot/bt_bot.py
+++ behavior_tree_bot/bt_bot.py
@@ -32,10 +32,11 @@ def setup_behavior_tree():
 
     spread_sequence = Sequence(name='Spread Strategy')
     neutral_planet_check = Check(if_neutral_planet_available)
-    spread_action = Action(spread_to_weakest_neutral_planet)
+    #closest_neutral_planet = Check()
+    spread_action = Action(spread_to_closest_neutral_planet)
     spread_sequence.child_nodes = [neutral_planet_check, spread_action]
 
-    root.child_nodes = [offensive_plan, spread_sequence, attack.copy()]
+    root.child_nodes = [spread_sequence,offensive_plan, attack.copy()]
 
     logging.info('\n' + root.tree_to_string())
     return root
